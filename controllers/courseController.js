1d290540961e30de18a472de18ac070eb57e0571
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-09-14 14:01:57 +0530

    Discharge user from course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -153,7 +153,31 @@ export const asignUser = (req, res) => {
 }
 
 export const dischargeUser = (req, res) => {
+    const { c_id, u_id } = req.body;
+    const existQuery = "select * from enrollments where c_id=? and u_id=?";
+    db.query(existQuery, [c_id, u_id], (err, enroll) => {
+        if (enroll.length === 0) {
+            res.status(404).json({
+                message: "Not found"
+            })
+        } else {
 
+            const discharge = "delete from enrollments where c_id=? and u_id=?";
+
+            db.query(discharge, [c_id, u_id], (err, data) => {
+                if (!err) {
+                    res.status(200).json({
+                        message: "User discharged from course"
+                    })
+                } else {
+                    res.status(500).json({
+                        message: "Internal server error",
+                        //err:err
+                    })
+                }
+            })
+        }
+    })
 }
 export const listUserCourses = (req, res) => {
 
