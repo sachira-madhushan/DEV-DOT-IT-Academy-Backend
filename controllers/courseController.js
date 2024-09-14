7c6a2c0adf069a228b19d31328db281562616f2f
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-09-14 13:49:37 +0530

    Assign user to course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,13 +1,13 @@
 import connectDB from "../config/database.js";
 
-const db=connectDB();
+const db = connectDB();
 
-export const addCourse=(req,res)=>{
-    const {c_title,c_description,c_price,c_instructor,c_banner,c_intro} =req.body;
+export const addCourse = (req, res) => {
+    const { c_title, c_description, c_price, c_instructor, c_banner, c_intro } = req.body;
 
-    const saveQuery="insert into courses(c_title,c_description,c_price,c_instructor,c_banner,c_intro) values(?)";
+    const saveQuery = "insert into courses(c_title,c_description,c_price,c_instructor,c_banner,c_intro) values(?)";
 
-    const values=[
+    const values = [
         c_title,
         c_description,
         c_price,
@@ -16,125 +16,145 @@ export const addCourse=(req,res)=>{
         c_intro
     ];
 
-    db.query(saveQuery,[values],(err,data)=>{
-        if(!err){
+    db.query(saveQuery, [values], (err, data) => {
+        if (!err) {
             res.status(201).json({
-                message:"Course created"
+                message: "Course created"
             })
-        }else{
+        } else {
             res.status(500).json({
-                message:"Internal server error"
+                message: "Internal server error"
             })
         }
     })
 }
-export const addChapter=(req,res)=>{
-    const {c_id,chap_title,chap_description,chap_video} =req.body;
-    const values=[
-        c_id,chap_title,chap_description,chap_video
+export const addChapter = (req, res) => {
+    const { c_id, chap_title, chap_description, chap_video } = req.body;
+    const values = [
+        c_id, chap_title, chap_description, chap_video
     ];
 
-    const existQuery="select * from courses where c_id=?";
-    
-    db.query(existQuery,[c_id],(err,data)=>{
-        if(data.length===0){
+    const existQuery = "select * from courses where c_id=?";
+
+    db.query(existQuery, [c_id], (err, data) => {
+        if (data.length === 0) {
             res.status(404).json({
-                message:"Course not found"
+                message: "Course not found"
             })
-        }else{
-            const saveQuery="insert into course_chapters(c_id,chap_title,chap_description,chap_video) values(?)";
-            
-            db.query(saveQuery,[values],(err,data)=>{
-                if(!err){
+        } else {
+            const saveQuery = "insert into course_chapters(c_id,chap_title,chap_description,chap_video) values(?)";
+
+            db.query(saveQuery, [values], (err, data) => {
+                if (!err) {
                     res.status(201).json({
-                        message:"New chaper added"
+                        message: "New chaper added"
                     })
-                }else{
+                } else {
                     res.status(500).json({
-                        message:"Internal server error",
+                        message: "Internal server error",
                     })
                 }
             })
         }
     })
-    
-    
 
-    
-    
 }
 
-export const deleteCourse=(req,res)=>{
-    const courseID=req.params['id'];
-    const existQuery="select * from courses where c_id=?";
-    db.query(existQuery,[courseID],(err,data)=>{
-        if(data.length===0){
+export const deleteCourse = (req, res) => {
+    const courseID = req.params['id'];
+    const existQuery = "select * from courses where c_id=?";
+    db.query(existQuery, [courseID], (err, data) => {
+        if (data.length === 0) {
             res.status(404).json({
-                message:"Course not found"
+                message: "Course not found"
             });
-        }else{
-            const deleteQuery="delete from courses where c_id=?";
-            db.query(deleteQuery,[courseID],(err,data)=>{
-                if(err){
+        } else {
+            const deleteQuery = "delete from courses where c_id=?";
+            db.query(deleteQuery, [courseID], (err, data) => {
+                if (err) {
                     res.status(500).json({
-                        message:"Internal server error"
+                        message: "Internal server error"
                     })
-                }else{
+                } else {
                     res.status(201).json({
-                        message:"Course deleted"
+                        message: "Course deleted"
                     })
                 }
-            })            
-        
+            })
+
         }
     })
 }
 
-export const updateCourse=(req,res)=>{
+export const updateCourse = (req, res) => {
 
 }
 
-export const listAllCourses=(req,res)=>{
-    const listQuery="select * from courses";
+export const listAllCourses = (req, res) => {
+    const listQuery = "select * from courses";
 
-    db.query(listQuery,[],(err,data)=>{
+    db.query(listQuery, [], (err, data) => {
         res.status(200).json({
-            course:data
+            course: data
         })
     })
 }
 
-export const viewCourse=(req,res)=>{
-    const courseID=req.params['id'];
+export const viewCourse = (req, res) => {
+    const courseID = req.params['id'];
 
-    const existQuery="SELECT * FROM courses WHERE c_id = ?;";
+    const existQuery = "SELECT * FROM courses WHERE c_id = ?;";
 
-    db.query(existQuery,[courseID],(err,data)=>{
+    db.query(existQuery, [courseID], (err, data) => {
 
-        if(data.length===0){
+        if (data.length === 0) {
             res.status(404).json({
-                message:"Course not found"
+                message: "Course not found"
             })
-        }else{
-            const chaptersQuery="SELECT * FROM course_chapters WHERE c_id = ?;";
-            db.query(chaptersQuery,[courseID],(err,chap)=>{
+        } else {
+            const chaptersQuery = "SELECT * FROM course_chapters WHERE c_id = ?;";
+            db.query(chaptersQuery, [courseID], (err, chap) => {
                 res.status(200).json({
-                    course:data,
-                    chapters:chap
+                    course: data,
+                    chapters: chap
                 })
             })
-            
+
         }
     })
 }
 
-export const asignUser=(req,res)=>{
+export const asignUser = (req, res) => {
+    const { c_id, u_id } = req.body;
+    const existQuery = "select * from enrollments where c_id=? and u_id=?";
+    db.query(existQuery, [c_id, u_id], (err, enroll) => {
+        if (enroll.length === 0) {
+            const assignQuery = "Insert into enrollments(c_id,u_id) values(?,?)";
+
+            db.query(assignQuery, [c_id, u_id], (err, data) => {
+                if (!err) {
+                    res.status(200).json({
+                        message: "User assigned to course"
+                    })
+                } else {
+                    res.status(500).json({
+                        message: "Internal server error",
+                        //err:err
+                    })
+                }
+            })
+        } else {
+            res.status(500).json({
+                message: "Already exist"
+            })
+        }
+    })
 
 }
 
-export const dischargeUser=(req,res)=>{
-    
+export const dischargeUser = (req, res) => {
+
 }
-export const listUserCourses=(req,res)=>{
+export const listUserCourses = (req, res) => {
 
 }
