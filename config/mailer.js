ebb35549fe5e0071bd68e0ccc38087a4a6168526
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-09-17 12:09:44 +0530

    Node Mailer Configurations

---
+++ config/mailer.js
@@ -0,0 +1,51 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+import hbs from 'nodemailer-express-handlebars';
+import path from 'path'
+
+dotenv.config();
+
+const sendPasswordEmail =(to,name,password,res)=>{
+    const transport=nodemailer.createTransport({
+        host:process.env.SMTP_SERVER,
+        port:465,
+        secure:true,
+        auth:{
+            user:process.env.SMTP_MAIL,
+            pass:process.env.MAIL_PASSWORD
+        }
+    })
+
+    const mailOptions={
+        from:process.env.SMTP_MAIL,
+        to:to,
+        subject:"DEV DOT IT Academy - Registraion Complete",
+        template: 'StudentRegistrationEmail',
+        context: {
+            name:name,
+            password:password,
+        },
+    }
+
+    const handlebarOptions = {
+        viewEngine: {
+          partialsDir: path.resolve('../models/'),
+          defaultLayout: false,
+        },
+        viewPath: path.resolve('../models/'),
+        extName: '.html',
+      };
+
+    transport.use('compile', hbs(handlebarOptions));
+
+    transport.sendMail(mailOptions,(err,info)=>{
+        if(err){
+            res.status(500).send(err+info)
+        }else{
+            res.status(200).send("Register Success!.Mail was sent successfuly!")
+        }
+    })
+
+}
+
+export default sendPasswordEmail
