061c3c3841db4ed89fbdaa66db36cf0a5ce88f56
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-10-04 12:19:12 +0530

    Email Updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,6 @@ export const registerUser = async (req, res) => {
     const u_password = generator.generate({
         length: 12,
         numbers: true,
-        symbols: true,
         lowercase: true,
         uppercase: true,
         strict: true
@@ -56,30 +55,35 @@ export const registerUser = async (req, res) => {
 //@access public
 export const loginUser = async (req, res) => {
     const { u_email, u_password } = req.body;
-    const existQuery = "select * from users where u_email=?";
+    const existQuery = "SELECT * FROM users WHERE u_email=?";
 
     db.query(existQuery, [u_email], async (err, data) => {
+        if (err) {
+            res.status(500).json({ message: "Database query error" });
+            return;
+        }
+
         if (data.length === 0) {
             res.status(401).json({
                 message: "Invalid email or password",
-            })
+            });
         } else {
-            if (await bcrypt.compare(u_password, data[0].u_password)) {
-                res.json(
-                    {
-                        u_username: data[0].u_username,
-                        u_fullname: data[0].u_fullname,
-                        u_birthday: data[0].u_birthday,
-                        u_phone: data[0].u_phone,
-                        u_email: data[0].u_email,
-                        token: generateToken(data[0].u_id)
-                    }
-                ).status(200);
+            const passwordMatch = await bcrypt.compare(u_password, data[0].u_password);
+            
+            if (passwordMatch) {
+                res.status(200).json({
+                    u_username: data[0].u_username,
+                    u_fullname: data[0].u_fullname,
+                    u_birthday: data[0].u_birthday,
+                    u_phone: data[0].u_phone,
+                    u_email: data[0].u_email,
+                    token: generateToken(data[0].u_id),
+                });
             } else {
-                res.json("Internal server error").status(500);
+                res.status(401).json({ message: "Invalid email or password" });
             }
         }
-    })
+    });
 }
 
 //@des Get Authenticated user details
