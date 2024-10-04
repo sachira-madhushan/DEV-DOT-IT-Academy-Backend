8bd103c01ab4841530667003613338c537500587
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-10-04 12:25:34 +0530

    Special Characters For Password

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,6 +14,7 @@ export const registerUser = async (req, res) => {
     const u_password = generator.generate({
         length: 12,
         numbers: true,
+        symbols: true,
         lowercase: true,
         uppercase: true,
         strict: true
